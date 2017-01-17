b4dcc4c678537cbb5765c03150383f511d7ae5cb
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-01-17 13:14:59 +0100

    Neo4J integration

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -5,6 +5,9 @@ import com.serli.oracle.of.bacon.repository.MongoDbRepository;
 import com.serli.oracle.of.bacon.repository.Neo4JRepository;
 import com.serli.oracle.of.bacon.repository.RedisRepository;
 import net.codestory.http.annotations.Get;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Path;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,46 +27,9 @@ public class APIEndPoint {
 
     @Get("bacon-to?actor=:actorName")
     public String getConnectionsToKevinBacon(String actorName) {
+       List<?> connectionsToKevinBacon = neo4JRepository.getConnectionsToKevinBacon(actorName);
 
-        return "[\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 85449,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Bacon, Kevin (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 2278636,\n" +
-                "\"type\": \"Movie\",\n" +
-                "\"value\": \"Mystic River (2003)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 1394181,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Robbins, Tim (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 579848,\n" +
-                "\"source\": 85449,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 9985692,\n" +
-                "\"source\": 1394181,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "}\n" +
-                "]";
+        return connectionsToKevinBacon.toString();
     }
 
     @Get("suggest?q=:searchQuery")
--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
@@ -1,26 +1,51 @@
 package com.serli.oracle.of.bacon.repository;
 
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Relationship;
 
 public class Neo4JRepository {
+
+    public static final String ACTOR = "Actor";
+    public static final String MOVIE = "Movie";
     private final Driver driver;
 
     public Neo4JRepository() {
-        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "admin"));
     }
 
     public List<?> getConnectionsToKevinBacon(String actorName) {
         Session session = driver.session();
+        List<GraphItem> graphItemList = new ArrayList<>();
 
-        // TODO implement Oracle of Bacon
-        return null;
+        String query = "MATCH p=shortestPath((bacon:Actor {name:'Bacon, Kevin (I)'})-[*]-(a:Actor {name:{actorName}})) RETURN p";
+        StatementResult result = session.run(query, Values.parameters("actorName", actorName));
+
+        for (Record record : result.list()) {
+            for (Node n : record.get("p").asPath().nodes()) {
+                List<String> types = iterableToList(n.labels());
+                if(types.contains(ACTOR)){
+                    graphItemList.add(new GraphNode(n.id(), n.get("name").toString(), ACTOR));
+                } else if(types.contains(MOVIE)){
+                    graphItemList.add(new GraphNode(n.id(), n.get("title").toString(), MOVIE));
+                }
+            }
+            for (Relationship r : record.get("p").asPath().relationships())
+                graphItemList.add(new GraphEdge(r.id(), r.startNodeId(), r.endNodeId(), r.type()));
+        }
+        return graphItemList;
+    }
+
+    private <T> List<T> iterableToList(Iterable<T> iterable) {
+        List<T> list = new ArrayList<T>();
+        for (T t : iterable) {
+            list.add(t);
+        }
+        return list;
     }
 
     private static abstract class GraphItem {
@@ -55,6 +80,12 @@ public class Neo4JRepository {
             this.value = value;
             this.type = type;
         }
+
+        @Override
+        public String toString() {
+            return String.format("{\"data\":" +
+                                    "{\"id\":%s, \"value\":%s, \"type\":\"%s\"}}", id, value, type);
+        }
     }
 
     private static class GraphEdge extends GraphItem {
@@ -67,6 +98,12 @@ public class Neo4JRepository {
             this.source = source;
             this.target = target;
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{\"data\":" +
+                                    "{\"id\":%s, \"source\":%s, \"target\":%s, \"value\":\"%s\"}}", id, source, target, value);
         }
     }
 }
