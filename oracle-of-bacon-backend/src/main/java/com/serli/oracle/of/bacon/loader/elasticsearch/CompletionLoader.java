ad6b8dab1c1ce20a6fb6f63cf2f6f88215405c4e
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-01-17 17:04:02 +0100

    Elastic Search insertion

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -2,11 +2,17 @@ package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
 import io.searchbox.client.JestClient;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Index;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CompletionLoader {
@@ -23,13 +29,40 @@ public class CompletionLoader {
         JestClient client = ElasticSearchRepository.createClient();
 
         try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
-            bufferedReader.lines()
+
+                List<Index> currentChunk = new ArrayList<>();
+
+                bufferedReader
+                    .lines()
                     .forEach(line -> {
-                        //TODO ElasticSearch insert
-                        System.out.println(line);
+
+                        Map<String, String> source = new LinkedHashMap<>();
+                        source.put("name", line);
+                        currentChunk.add(new Index.Builder(source).build());
+
+                        if(currentChunk.size() == 50000) {
+                            insertChunk(client, currentChunk);
+                        }
                     });
+                if(!currentChunk.isEmpty()) {
+                    insertChunk(client, currentChunk);
+                }
         }
 
         System.out.println("Inserted total of " + count.get() + " actors");
+    }
+
+    private static void insertChunk(JestClient client, List<Index> currentChunk) {
+        try {
+            client.execute(new Bulk.Builder()
+                    .defaultIndex("bacon")
+                    .defaultType("actor")
+                    .addAction(currentChunk)
+                    .build());
+            System.out.println(count.addAndGet(currentChunk.size()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        currentChunk.clear();
     }
 }
